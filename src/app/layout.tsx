360fadba2baf3d2a893316d6aff5e1c28a271585
augustodrp <augustodrp@gmail.com>
2024-01-28 22:34:47 -0300

    #2 Main screen - Concept (Menu, Footer and main)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
- import { Inter } from "next/font/google";
 import "./globals.css";
-
- const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Spotify",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-zinc-800 text-zinc-50">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <main></main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">
+          sidebar
+        </aside>
+        <main className="flex-1 p-6">
+          main
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
   );
 }
