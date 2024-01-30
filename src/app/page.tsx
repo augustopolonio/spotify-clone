f0ddbef96afb61a9be6bf4708c25495c4db37cad
augustodrp <augustodrp@gmail.com>
2024-01-30 13:59:50 -0300

    Increase the sidebar width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
+        <aside className="w-72 bg-zinc-950 p-6">
           sidebar
         </aside>
         <main className="flex-1 p-6">
