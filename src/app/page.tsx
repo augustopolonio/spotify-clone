78ad4cb2a2fdc88ddf639a9bdd662ddde5d5eef5
augustopolonio <augustodrp@gmail.com>
2024-02-02 17:01:34 -0300

    #4 Add a grid with albuns and libraries in main view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as HomeIcon, Search, Library } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -35,7 +36,62 @@ export default function Home() {
 
         </aside>
         <main className="flex-1 p-6">
-          main
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+            <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/Aerosmith_nine_lives.jpg" alt="Aerosmith album" width={104} height={104}></Image>
+              <strong>Aerosmith - Nine Lives</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 bg-green-500 rounded-full text-black ml-auto mr-4 invisible group-hover:visible">
+                <Play />
+              </button>              
+            </a>
+          </div>
+
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
