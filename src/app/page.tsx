316ab8317d25b0c3028b5ebd881a17cbaadb13dc
augustopolonio <augustodrp@gmail.com>
2024-02-05 18:56:41 -0300

    #8: Fix the wrong update on previous commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="flex items-center gap-2">
             <div className="w-3 h-3 bg-red-500 rounded-full"></div>
             <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-            <div className="w-3 h-3 bg-green-500 rounded-full grow-0 shrink-0"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
           </div>
 
           <nav className="space-y-5 mt-10">
